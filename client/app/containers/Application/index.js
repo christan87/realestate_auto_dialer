fe332ccc3b17af57a36e10b60d45c1f1633066c3
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2023-07-08 02:36:02 -0700

    created the application and homepage containers along with my root reducers and actions files plus the store so that I can mange state redux

---
+++ client/app/containers/Application/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import { connect } from 'react-redux';
+import { Switch, Route } from "react-router-dom";
+import { Container } from "reactstrap";
+
+import actions from "../../actions";
+
+//routes
+import HomePage from "../HomePage";
+import Navigation from "../Navigation";
+import Login from "../Login";
+import Authentication from "../Authentication";
+import Dashboard from "../Dashboard";
+
+import Page404 from '../../components/Common/Page404';
+import Footer from "../../components/Common/Footer";
+
+class Application extends React.PureComponent {
+
+    componentDidMount() {
+        const token = localStorage.getItem('token');
+
+        if(token) {
+            this.props.fetchProfile()
+        }
+    }
+
+    render() {
+        return (
+            <div className="application">
+                {/* <Navigation /> */}
+                <main className="main">
+                    <Container>
+                        <div className="wrapper">
+                            <Switch>
+                                <Route exact path='/' component={HomePage} />
+
+                                {/*
+                                <Route exact path='/login' component={Login} />
+                                <Route 
+                                    path='/dashboard'
+                                    component={Authentication(Dashboard)}
+                                />
+                                <Route path='/404' component={Page404} />
+                                <Route path='*' component={Page404} /> 
+                                */}
+                            </Switch>
+                        </div>
+                    </Container>
+                </main>
+                {/* <Footer /> */}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {};
+};
+
+export default connect(mapStateToProps, actions)(Application);
---
+++ client/app/containers/HomePage/index.js
@@ -0,0 +1,26 @@
+/*
+* Homepage
+*/
+
+import React from "react";
+
+import { connect } from "react-redux";
+import { Row, Col } from 'reactstrap';
+
+import actions from '../../actions';
+
+class Homepage extends React.PureComponent {
+    render() {
+        return (
+            <div className="homepage">
+               
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {};
+};
+
+export default connect(mapStateToProps, actions)(Homepage);
